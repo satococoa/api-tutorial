b3c95a013c09e398615925549e1f44820cd85c91
Satoshi Ebisawa <e.satoshi@gmail.com>
2013-07-10 15:21:36 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130710053649) do
+ActiveRecord::Schema.define(version: 20130710062107) do
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.string   "name"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "rails_admin_histories", force: true do |t|
     t.text     "message"
