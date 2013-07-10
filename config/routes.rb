f0b69990f88cac24f5d94ea6cf508a9737db1cba
Satoshi Ebisawa <e.satoshi@gmail.com>
2013-07-10 14:37:10 +0900

    r g rails_admin:install --skip-devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Bbs::Application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
