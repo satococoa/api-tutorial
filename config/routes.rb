d508d3c0e80078f446b56445faf2cd06ebcb985c
Satoshi Ebisawa <e.satoshi@gmail.com>
2013-07-24 14:01:53 +0900

    Add routes for /api/v1/posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Bbs::Application.routes.draw do
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   root to: 'rails_admin/main#dashboard'
+
+  namespace :api do
+    namespace :v1 do
+      resources :posts, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
 end
