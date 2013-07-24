d061fb0673309e8379b910ebf46781b7938ec452
Satoshi Ebisawa <e.satoshi@gmail.com>
2013-07-24 17:05:59 +0900

    Add show action

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Bbs::Application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :posts, only: [:index, :create, :update, :destroy]
+      resources :posts, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
