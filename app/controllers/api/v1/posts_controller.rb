f23b8f67229f1c5cc00270dc1474af6cca9aa378
Satoshi Ebisawa <e.satoshi@gmail.com>
2013-07-31 17:58:40 +0900

    Implement posts methods

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,5 +1,36 @@
 class Api::V1::PostsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     @posts = Post.all
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      head :created
+    else
+      render json: @post.errors.messages.keys, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      head :ok
+    else
+      render json: @post.errors.messages.keys, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    head :ok
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :name, :body)
   end
 end
--- spec/requests/api/v1/api_v1_posts_spec.rb
+++ spec/requests/api/v1/api_v1_posts_spec.rb
@@ -25,6 +25,108 @@ describe "Api::V1::Posts" do
         expect(data.keys).to match_array(['title', 'name', 'body'])
       end
     end
+  end
 
+  describe "POST /api/v1/posts" do
+    context '正しいパラメータを送ったとき' do
+      let(:params) {
+        {
+            post: FactoryGirl.attributes_for(:post),
+            format: :json
+        }
+      }
+      it 'ステータスコード201を返す' do
+        post api_v1_posts_path, params
+        expect(response.status).to eq(201)
+      end
+      it '投稿が保存できる' do
+        #before_count = Post.count
+        #post api_v1_posts_path, params
+        #after_count = Post.count
+        #expect( (before_count+1) == after_count ).to be_true
+
+        expect {
+          post api_v1_posts_path, params
+        }.to change { Post.count }.by(1)
+      end
+    end
+
+    context '不正なパラメータを送ったとき' do
+      let(:params) {
+        {
+            post: FactoryGirl.attributes_for(:post, name: 'a'),
+            format: :json
+        }
+      }
+      it 'ステータスコード422を返す' do
+        post api_v1_posts_path, params
+        expect(response.status).to eq(422)
+      end
+      it 'エラーメッセージが返る' do
+        post api_v1_posts_path, params
+        expect(response.body).not_to eq('')
+      end
+    end
+  end
+
+  describe 'PATCH /api/v1/posts/:id' do
+    let(:post) { FactoryGirl.create(:post) }
+
+    context '正しいパラメータを送ったとき' do
+      let(:params) {
+        {
+            post: FactoryGirl.attributes_for(:post),
+            format: :json
+        }
+      }
+      it 'ステータスコードは200が返る' do
+        patch api_v1_post_path(post), params
+        expect(response.status).to eq(200)
+      end
+      it '値が更新される' do
+        #before_title = Post.find(post.id).title
+        #patch api_v1_post_path(post), params
+        #after_title = Post.find(post.id).title
+        #expect(before_title != after_title).to be_true
+
+        expect {
+          patch api_v1_post_path(post), params
+        }.to change { Post.find(post.id).title }.from(post.title).to(params[:post][:title])
+      end
+    end
+
+    context '不正なパラメータを送ったとき' do
+      let(:params) {
+        {
+            post: FactoryGirl.attributes_for(:post, name: 'a'),
+            format: :json
+        }
+      }
+      it 'ステータスコードは422が返る' do
+        patch api_v1_post_path(post), params
+        expect(response.status).to eq(422)
+      end
+
+      it '値が更新されない' do
+        expect {
+          patch api_v1_post_path(post), params
+        }.not_to change { Post.find(post.id).title }
+      end
+    end
+  end
+
+  describe 'DELETE /api/v1/posts/:id' do
+    let!(:post) { FactoryGirl.create(:post) }
+
+    it '削除される' do
+      #before_count = Post.count
+      #delete api_v1_post_path(post)
+      #after_count = Post.count
+      #expect((before_count-1) == after_count).to be_true
+
+      expect {
+        delete api_v1_post_path(post)
+      }.to change { Post.count }.by(-1)
+    end
   end
 end
